c96079eaa23d5e21568b7f1a5b70495bbc4519cc
DivyaPawar17 <163241392+DivyaPawar17@users.noreply.github.com>
2024-09-01 01:31:53 +0530

    Add files via upload

---
+++ Java Assignment/Myclass9.java
@@ -0,0 +1,25 @@
+Package Demo
+import java.util.Arrays;
+public class Myclass9
+{
+ public static void main(String[] args)
+ {
+ int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+ int max = arr[0];
+ for (int i = 1; i < arr.length; i++)
+ {
+ if (arr[i] > max)
+ {
+ max = arr[i];
+ }
+ }
+ double sum = 0;
+ for (int i = 0; i < arr.length; i++)
+ {
+ sum += arr[i];
+ }
+ double avg = sum / arr.length;
+ System.out.println("Maximum Element: " + max);
+ System.out.println("Average of Elements: " + avg);
+ }
+}
